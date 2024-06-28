3c46c73d8b76d6e2fdd6657a9f053d390cb8ecb3
Rebecca <rebecca@Rebeccas-MacBook-Air.local>
2024-06-28 17:35:17 -0400

    Nav links indicate path

---
+++ app/ui/nav.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import Link from 'next/link';
+import Navlinks from '@/app/ui/navlinks'
+import { usePathname } from 'next/navigation';
+import clsx from 'clsx';
+
+const links = [
+	{name: 'Shop', href:'/shop'},
+	{name: 'Learn', href:'/learn'},
+	{name: 'About', href:'/about'},
+];
+
+export default function Nav() {
+	const pathname = usePathname();
+
+	return (
+		<>
+			{links.map((link) => {
+				return (
+					<Link 
+						key={link.name}
+						href={link.href}
+						className={clsx(
+							'block mt-4 lg:inline-block lg:mt-0 text-black mr-4',
+							{
+								'underline': pathname === link.href,
+							}
+						)}
+					>
+					{link.name}
+					</Link>
+				);
+			})}
+		</>
+	);
+}
