bee66bf07dac62dc583ed2ad8da0263a9a343644
Rebecca <rebecca@Rebeccas-MacBook-Air.local>
2024-10-03 18:51:15 -0400

    Shop dropdown

---
+++ app/ui/header/shopdropdown.tsx
@@ -0,0 +1,60 @@
+'use client';
+
+import Link from 'next/link';
+import { useState } from 'react';
+import clsx from 'clsx';
+
+const links = [
+    {name: 'Handmade', href:'/shop/handmade'},
+    {name: 'Felting Kits', href:'/shop/diy'},
+    {name: 'Custom Orders', href:'/shop/custom'},
+    {name: 'Seasonal', href:'/shop/new'},
+    {name: 'Shop All', href:'/shop'}
+];
+
+export default function ShopDropdown( {pathname} ) {
+	const [toggle, setToggle] = useState("hidden");
+
+	function menuToggle() {
+		if (toggle == "hidden") {
+			setToggle("block");
+		}
+		else {
+			setToggle("hidden");
+		}
+	}
+
+	return (
+		<div className="lg:inline-block relative m-2">
+			<button 
+				className={clsx(
+					'p-2 font-bold bg-white border border-milkweed rounded hover:bg-ladycardinal',
+					{
+						'underline': pathname.startsWith("/shop"),
+					}
+				)}
+				onClick={menuToggle}
+			>
+			Shop
+			</button>
+			<div 
+				className={"bg-white rounded w-[164px] absolute z-10 my-1 p-2 border border-ladycardinal shadow " + toggle}
+				onMouseLeave={menuToggle}
+				>
+				{links.map((link) => {
+	                return (
+	                    <>
+	                        <Link 
+	                            key={link.name}
+	                            href={link.href}
+	                            className="block text-black hover:text-black hover:bg-paper"
+	                        >
+	                        {link.name}
+	                        </Link>
+	                    </>
+	                );
+	            })}
+			</div>
+		</div>
+	)
+}
