03ccbd907dd6ce733289d1482ae0574e0c95afaf
Rebecca <rebecca@Rebeccas-MacBook-Air.local>
2024-10-03 19:33:26 -0400

    Minor edit

--- app/ui/home/newpreview.tsx
+++ app/ui/home/newpreview.tsx
@@ -11,7 +11,7 @@ export default async function NewPreview() {
         <ProductList products={products}/>
       </div>
       <div className="text-center m-2">
-        <HomePreviewButton url="/shop/sale" text="Show me more" /> 
+        <HomePreviewButton url="/shop/seasonal" text="Show me more" /> 
       </div>
     </div>
     );
