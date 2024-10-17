2f6336294720f6cefc046118094c8982bb658d3e
Rebecca <rebecca@Rebeccas-MacBook-Air.local>
2024-10-17 16:58:54 -0400

    Trying again

--- app/ui/shop/custom/commission.tsx
+++ app/ui/shop/custom/commission.tsx
@@ -8,7 +8,7 @@ export default function Commission() {
         <div>
         <h2>Request a Custom Order</h2>
         <p className='text-sm'>Form not loading? <ExternalLink url="https://tally.so/r/3j7NEx" text="Open the form in a new tab" /></p>
-            <iframe data-tally-src="https://tally.so/embed/3j7NEx?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1" loading="lazy" width="100%" height="949" frameBorder="0" marginheight="0" marginwidth="0" title="Commissions"></iframe>
+            <iframe data-tally-src="https://tally.so/embed/3j7NEx?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1" loading="lazy" width="100%" height="949" frameBorder="0" marginHeight="0" marginWidth="0" title="Commissions"></iframe>
 
             <Script
                 id="tally-js"
