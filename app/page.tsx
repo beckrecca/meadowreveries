009034d2bd619dac14b835b6ba09f8ea20528eed
Rebecca <rebecca@Rebeccas-MacBook-Air.local>
2024-12-16 11:46:04 -0500

    Updated sale and shipping messaging

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,9 @@ export default function Home() {
   return (
     <div>
       <main className="pb-4">
+        <div className='bg-goldenrod p-4 m-auto text-center'>
+          <strong>Holiday shipping notice:</strong> USPS recommends shipping by December 18, 2024 for delivery before Christmas Day. <br/><span className='text-sm'>Orders placed after December 16 are not guaranteed to arrive in time for the holiday.</span>
+        </div>
         <HomeBanner />
         <WinterPreview />
         <CustomPreview />
