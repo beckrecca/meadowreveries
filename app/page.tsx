36cef1b8335a39ed5a585f1769ee6d5c1ed5957c
Rebecca Doris <rdoris@Rebeccas-MacBook-Pro.local>
2025-05-27 19:18:34 -0400

    taking down sale promo

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import StaticHomeBanner from '@/app/ui/home/statichomebanner';
 import Subscribeform from '@/app/ui/subscribeform';
 import DiyPreview from '@/app/ui/home/diypreview';
 import CustomPreview from '@/app/ui/home/custompreview';
-import SalePreview from '@/app/ui/home/salepreview';
+//import SalePreview from '@/app/ui/home/salepreview';
 import PetPreview from '@/app/ui/home/petpreview';
 import EventPreview from '@/app/ui/home/eventpreview';
 
@@ -14,9 +14,8 @@ export default function Home() {
           image='/homepage_milkweed_meadows.png' 
           alt='Banner image showing a mowed path in tall meadow grasses with pink milkweed flowers in full bloom.' 
           />
-        <SalePreview />
-        <PetPreview />
         <DiyPreview />
+        <PetPreview />
         <CustomPreview />
         <Subscribeform />
       </main>
