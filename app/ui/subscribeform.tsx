b2c4f6a84c76e719b37bf5a2a845099a25528931
Rebecca Doris <rdoris@Rebeccas-MacBook-Pro-2.local>
2025-06-18 09:11:34 -0400

    Disabling shop for summer

--- app/ui/subscribeform.tsx
+++ app/ui/subscribeform.tsx
@@ -5,7 +5,7 @@ export default function Subscribeform() {
 	return (
 		<div className="bg-paper w-[336px] p-2 mx-auto rounded border border-paper shadow">
           <div className="bg-goldenrod p-2 rounded">
-            <h2 className="my-2 text-center text-2xl"><strong>We're still learning to fly</strong></h2>
+            <h2 className="my-2 text-center text-2xl"><strong>Borbs for your inbox</strong></h2>
           </div>
           <Image 
 	          src="/subscribe_fledge.png"
